3c4256ff773ab312da6668354d662c2ae0524ba2
Pavel Klyagin <kapver@gmail.com>
2016-04-06 13:11:20 +0300

    added upwork/php-upwork

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'phpDocumentor' => array($vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'Upwork\\API' => array($vendorDir . '/upwork/php-upwork/src'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log'),
     'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
