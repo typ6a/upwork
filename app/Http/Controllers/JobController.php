8790b2de6714ecedd5eb260b68bd4e4826f8cbc9
Dmitry Znak <znakdmitry@gmail.com>
2016-04-12 11:39:32 +0300

    trying to get oAuth works

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -9,18 +9,23 @@ use Illuminate\Support\Facades\Session;
 class JobController extends Controller {
     
     public function find(){
+        
+        define('STDIN',fopen("php://stdin","r"));
+        
+        session_start();
         $data = [
-            'consumerKey'       => '187335280ef0ccdacb1b146edf110d98',  // SETUP YOUR CONSUMER KEY
-            'consumerSecret'    => 'ca9f909c8dc55fd7',                  // SETUP KEY SECRET
-            'accessToken'       => Session::get('access_token'),        // got access token
-            'accessSecret'      => Session::get('access_secret'),       // got access secret
+            'consumerKey'       => '47696c9412b3f5875f56494e812af800',  // SETUP YOUR CONSUMER KEY
+            'consumerSecret'    => 'e8b4f9ddf17edbf1',                  // SETUP KEY SECRET
+            //'accessToken'       => Session::get('access_token'),        // got access token
+            //'accessSecret'      => Session::get('access_secret'),       // got access secret
             'debug'             => true,                                // enables debug mode
         ];
         $config = new \Upwork\API\Config($data);
         $client = new \Upwork\API\Client($config);
         $accessTokenInfo = $client->auth();
-        pre($accessTokenInfo,1);
+        pre ($accessTokenInfo,1);
         
+        pre($accessTokenInfo,1);
         $jobs = new \Upwork\API\Routers\Jobs\Search($client);
         $jobs->find([
             'q' => 'something to request',
