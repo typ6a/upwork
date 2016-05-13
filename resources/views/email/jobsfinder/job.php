c6452ccd5733b411197d7c0f58e5ec9f3a01d46c
Dmitry Znak <znakdmitry@gmail.com>
2016-05-13 14:55:13 +0300

    13 friday

--- resources/views/email/jobsfinder/job.php
+++ resources/views/email/jobsfinder/job.php
@@ -1,6 +1,7 @@
-<h1><?= $job->title ?></h1>
+<h3><?= $job->title ?></h3>
 <p><?= $job->url ?></p>
 <p><?= $job->snippet ?></p>
 <p><?= $job->client->country ?></p>
 <p><?= implode($job->skills) ?> </p>
+<p><?= $job->budget ?></p>
 <hr/>
