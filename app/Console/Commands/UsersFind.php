6115d95fbe29bc31fd98c8853cd0238bd951da88
Dmitry Znak <znakdmitry@gmail.com>
2016-09-22 17:58:00 +0300

    twitter twitter cfg added 1.0

--- app/Console/Commands/UsersFind.php
+++ app/Console/Commands/UsersFind.php
@@ -38,15 +38,64 @@ class UsersFind extends Command
     public function handle()
     {
         $exc = null;
+        $page = 1;
+        $checkedUsers[]= [
+            'id' => 'id ',
+            'name' => 'name ',
+            'screen_name' => 'screen_name ',
+            'location' => 'location ',
+            'description' => 'description '
+        ];
         while(is_null($exc)){
             try {
                 $users = \Twitter::getUsersSearch([
                     'q' => 'honda civic',
-                    'page' => 52,
+                    'page' => $page,
                     'count' => 20
                 ]);
-                pre($users,1);
+                //pre($users[9],1);
+                //pre($users[9]->screen_name,1);
+
+                //pre(gettype($users[9]->name),1);
+
+                foreach ($users as $user){
+               //gettype($user);
+
+                    if (stristr($user->description, 'honda civic'))
+                    {
+                        $checkedUsers[] = [
+                            'id' => $user->id,
+                            'name' => $user->name,
+                            'screen_name' => $user->screen_name,
+                            'location' => $user->location,
+                            //'description' => trim(preg_replace('/\s\s+/', ' ', $user->description)),
+                            'description' => trim(preg_replace("/\r|\n/", ' ', $user->description))
+
+
+                        ];
+                    };
+                }
+                sleep (1);
+
+                $page++;
+
+                //pre($checkedUsers[9],1);
+
+
+//pre($checkedUsers,1);
+                    //echo $checkedUsers->name, "\n", $checkedUsers->description, "\n";
+                    //echo "-------------", "\n";
+
+                $fp = fopen('d://file.csv', 'w');
+
+                foreach ($checkedUsers as $checkedUser) {
+
+                    fputcsv($fp, $checkedUser);
+                }
+
+                fclose($fp);
+
             } catch (Exception $exc) {}
-        }
+        }return;
     }
 }
