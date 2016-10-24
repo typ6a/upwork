6257bd0b418202c4d42f11338755ba1f0663db6b
Dmitry Znak <znakdmitry@gmail.com>
2016-10-24 16:31:43 +0300

    continue jobs finder with no api

--- app/Console/Commands/WorkFind.php
+++ app/Console/Commands/WorkFind.php
@@ -18,14 +18,72 @@ class WorkFind extends Command
 
     public function handle()
     {
-        $this->findWork();
+        $html = '';
+        $jobs = $this->jobs();
+        //pre ($jobs,1);
+        foreach ($jobs as $job) {
+            $filename = $job['id'] . '.json';
+            //pre($filename, 1);
+            $filepath = 'd:\workspace\upwork\storage\app\upwork\\' . $filename;
+            $res = false;
+            if (!file_exists($filepath)) {
+                $res = file_put_contents($filepath, json_encode($job));
+                //pre($res,1);
+                    $html .= view('email.jobsfinder.job', ['job' => $job]);
+            }
+        }
+            pre($html,1);
+        $res = $this->sendEmail(
+            \Config::get('mail.from.address'), \Config::get('mail.from.name'), [
+            'kapver@gmail.com',
+            'znakdmitry@gmail.com'], 'JOBS FROM UPWORK', $html
+        );
+        exit('YAHOO!!! YAHOO!!! YAHOO!!! YAHOO!!! YAHOO!!! YAHOO!!! YAHOO!!!');
     }
 
-    public function findWork()
+
+    protected function sendEmail($from, $from_name, $recipients, $subject, $message, $attachmentFile = false)
+    {
+        $api_key = env('SENDGRID_API_KEY');
+        
+        $options = array('turn_off_ssl_verification' => $this->isSSLAvailable());
+
+        $email = new \SendGrid\Email();
+        $email->setSmtpapiTos(array_values($recipients));
+        $email->setFrom($from);
+        $email->setFromName($from_name);
+        $email->setSubject($subject);
+        $email->setHtml($message);
+        if (!empty($attachmentFile)) {
+            if (is_object($attachmentFile) && ($attachmentFile instanceof FileAttachment)) {
+                $email->addAttachment($attachmentFile->getPath(), $attachmentFile->getReadableName());
+            } else {
+                $email->addAttachment($attachmentFile);
+            }
+        }
+        $sendgrid = new \SendGrid($api_key, $options);
+        $response = $sendgrid->send($email);
+        $code = $response->getCode();
+        if ((int)$code === 200) {
+            return true;
+        }
+        return false;
+    }
+
+    protected function isSSLAvailable()
+    {
+        return defined('CURL_SSLVERSION_SSLv3') ? false : true;
+    }
+
+    public function jobs()
     {
         $baseUrl = 'https://www.upwork.com/o/jobs/browse/?q=crawl&sort=create_time%2Bdesc';
-        $html = file_get_contents($baseUrl);
-        $html = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $html);
+        $baseUrl = 'https://www.upwork.com/o/jobs/browse/?page=2&q=scrape&sort=create_time%2Bdesc';
+        //$html = file_get_contents($baseUrl);
+        //$html = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $html);
+        //file_put_contents('d:/upwork.html', $html);
+        //exit;
+        $html = file_get_contents('d:/upwork.html');
         $crawler = new Crawler($html);
         $jobBlockItems = $crawler->filter('.row .col-sm-12.jobs-list .job-tile');
 //pre($jobBlockItems, 1);
@@ -37,8 +95,12 @@ class WorkFind extends Command
                 $jobDescription = trim($jobBlockCrawler->filter('.description.break  div')->text());
                 $jobType = preg_replace('/\s+/', ' ', trim($jobBlockCrawler->filter('.text-muted.display-inline-block.m-sm-bottom.m-sm-top')->text()));
                 $jobUrl = $jobBlockCrawler->filter('h2 > a')->attr('href');
-                $jobId = preg_replace("/o/jobs/job/", '', $jobBlockCrawler->filter('h2 > a')->attr('href'));
-//pre($jobUrl,1);/o/jobs/job/_~017b1759de70734a52/
+
+                $jobId = preg_replace('#^.+(_\~[^\/]+).*$#', '$1', $jobUrl);
+                $jobUrl = 'https://www.upwork.com/jobs/' . $jobId . '/';
+                //$jobLocation = $jobBlockCrawler->filter('.glyphicon.glyphicon-md.air-icon-location.m-0 .text-muted.client-location')->text();
+                //pre ($jobUrl, 1);
+
                     $jobs[$jobUrl] = [
                         'title' => $jobTitle,
                         'description' => $jobDescription,
@@ -48,24 +110,8 @@ class WorkFind extends Command
                     ];
 
             }
-                    pre($jobs,1);
-            $categories = [];
-            foreach ($blockItems as $key => $mainCategoryBlock) {
-                $mainCategoryBlockCrawler = new Crawler($mainCategoryBlock);
-                $mainCategoryEl = $mainCategoryBlockCrawler->filter('h3 > a');
-                $mainCategoryTitle = $mainCategoryEl->text();
-                $mainCategoryUrl = $this->base_url . $mainCategoryEl->attr('href');
-                if(!in_array($mainCategoryTitle, ['Klantenservice'])){
-                    $categories[$mainCategoryTitle] = [
-                        'url' => $mainCategoryUrl,
-                        'title' => $mainCategoryTitle,
-                        'categories' => $this->parseSubCategories($mainCategoryBlockCrawler)
-                    ];
-                }
-            } 
-
-        pre($html, 1);
-
-
+            
+                    //pre($jobs,1);
+        return $jobs;
     }
 }
